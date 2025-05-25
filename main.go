3052546dcc6c51bd721d25b7778ba2d644a2dfcb
peng.peng <peng.peng@ucloud.cn>
2025-05-25 13:22:08 +0800

    chore: update

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 	cli "github.com/urfave/cli/v2"
 
-	"github.com/sliveryou/goctl-swagger/action"
-	"github.com/sliveryou/goctl-swagger/generate"
+	"github.com/pokitpeng/goctl-swagger/action"
+	"github.com/pokitpeng/goctl-swagger/generate"
 )
 
 var (
